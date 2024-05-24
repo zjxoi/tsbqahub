41a7656d63988f48297cb67e8d9920ce23707c65
Marco <marco.martin@sandbox.game>
2024-05-24 15:32:20 -0300

    Copy button functionality, new sections and content.

--- external.js
+++ external.js
@@ -1,3 +1,41 @@
-function copyText() {
-    
+function copyAlias1() {
+
+    let alias = document.getElementById("11prod").innerText
+    navigator.clipboard.writeText(alias)
+
+}
+
+function copyAlias2() {
+
+    let alias = document.getElementById("11demo").innerText
+    navigator.clipboard.writeText(alias)
+
+}
+
+function copyAlias3() {
+
+    let alias = document.getElementById("11dev").innerText
+    navigator.clipboard.writeText(alias)
+
+}
+
+function copyAlias4() {
+
+    let alias = document.getElementById("10prod").innerText
+    navigator.clipboard.writeText(alias)
+
+}
+
+function copyAlias5() {
+
+    let alias = document.getElementById("10demo").innerText
+    navigator.clipboard.writeText(alias)
+
+}
+
+function copyAlias6() {
+
+    let alias = document.getElementById("10dev").innerText
+    navigator.clipboard.writeText(alias)
+
 }
